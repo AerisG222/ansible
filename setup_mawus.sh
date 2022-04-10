37a19c156dc00df4bde5b008cbaade8c4b996baf
Mike Morano <mmorano@mikeandwan.us>
2022-04-10 11:09:13 -0400

    make script more dynamic

--- setup_mawus.sh
+++ setup_mawus.sh
@@ -3,17 +3,32 @@
 
 MAWUS_ENV=$(get_mawus_env)
 HOST=$(get_value "Workstation hostname (must exist in inventory file): " 'n')
+FIRST_RUN=$(get_value "First Run - need to prompt for sshd password?  (y/N): " 'n' 'n')
+VERBOSE=$(get_value "Verbose?  (y/N): " 'n' 'n')
+INVENTORY="inventories/inventory_mawus_${MAWUS_ENV}.yml"
+PLAYBOOK="playbook_mawus_${MAWUS_ENV}.yml"
+PLAYBOOK_ARGS=("--ask-become-pass")
+
+if [ "${FIRST_RUN}" == 'y' ]; then
+    PLAYBOOK_ARGS+=("--ask-pass")
+fi
+
+if [ "${VERBOSE}" == 'y' ]; then
+    PLAYBOOK_ARGS+=("-vvv")
+fi
+
+PLAYBOOK_ARGS+=("--inventory")
+PLAYBOOK_ARGS+=("${INVENTORY}")
+PLAYBOOK_ARGS+=("--limit")
+PLAYBOOK_ARGS+=("${HOST}")
+PLAYBOOK_ARGS+=("${PLAYBOOK}")
+
+echo ''
+
 DO_RUN=$(get_value "Configure -[${MAWUS_ENV}]- environment for mikeandwan.us on -[${HOST}]-?  (y/N): " 'n' 'n')
 
 if [ "${DO_RUN}" != 'y' ]; then
     exit
 fi
 
-INVENTORY="inventories/inventory_mawus_${MAWUS_ENV}.yml"
-PLAYBOOK="playbook_mawus_${MAWUS_ENV}.yml"
-
-ansible-playbook \
-    --ask-become-pass \
-    --inventory "${INVENTORY}" \
-    --limit "${HOST}" \
-    "${PLAYBOOK}"
+ansible-playbook "${PLAYBOOK_ARGS[@]}"
