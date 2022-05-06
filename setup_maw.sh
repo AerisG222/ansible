e5bc76d6cb66c7f6374a94b3ed8d7064172cc683
Mike Morano <mmorano@mikeandwan.us>
2022-05-06 14:20:26 -0400

    add tags to allow targeting primary configuration steps - and hardcode some prompts for now to simplify testing

--- setup_maw.sh
+++ setup_maw.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 . ./_shared.sh
 
-MAW_ENV=$(get_maw_env)
-HOST=$(get_value "Hostname (must exist in inventory file): " 'n')
-FIRST_RUN=$(get_value "First Run - need to prompt for sshd password?  (y/N): " 'n' 'n')
-VERBOSE=$(get_value "Verbose?  (y/N): " 'n' 'n')
+MAW_ENV=dev # $(get_maw_env)
+HOST=barett # $(get_value "Hostname (must exist in inventory file): " 'n')
+FIRST_RUN=n # $(get_value "First Run - need to prompt for sshd password?  (y/N): " 'n' 'n')
+VERBOSE=n   # $(get_value "Verbose?  (y/N): " 'n' 'n')
+TAGS=maw_services # $(get_value "Run specific tag?  (maw_backups | maw_host | maw_services): " 'n' 'n')
 PLAYBOOK_ARGS=("--ask-become-pass")
 
 if [ "${MAW_ENV}" == "prod" ]; then
@@ -21,17 +22,22 @@ if [ "${VERBOSE}" == 'y' ]; then
     PLAYBOOK_ARGS+=("-vvv")
 fi
 
+if [ "${TAGS}" == 'maw_backups' -o "${TAGS}" == 'maw_host' -o "${TAGS}" == 'maw_services' ]; then
+    PLAYBOOK_ARGS+=("--tags")
+    PLAYBOOK_ARGS+=("${TAGS}")
+fi
+
 PLAYBOOK_ARGS+=("--inventory")
 PLAYBOOK_ARGS+=("${INVENTORY}")
 PLAYBOOK_ARGS+=("--limit")
-PLAYBOOK_ARGS+=("${HOST}")
+PLAYBOOK_ARGS+=("maw_current_prod,${HOST}")
 PLAYBOOK_ARGS+=("--extra-vars")
 PLAYBOOK_ARGS+=("@vars/maw_${MAW_ENV}.yml")
 PLAYBOOK_ARGS+=("playbook_maw.yml")
 
 echo ''
 
-DO_RUN=$(get_value "Configure -[${MAW_ENV}]- environment for mikeandwan.us on -[${HOST}]-?  (y/N): " 'n' 'n')
+DO_RUN=y # $(get_value "Configure -[${MAW_ENV}]- environment for mikeandwan.us on -[${HOST}]-?  (y/N): " 'n' 'n')
 
 if [ "${DO_RUN}" != 'y' ]; then
     exit
