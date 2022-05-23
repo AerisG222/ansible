4b73e0867a952c4b571b26f505b13414a345d17f
Mike Morano <mmorano@mikeandwan.us>
2022-05-23 11:16:59 -0400

    update reverse geocode task based on upstream container - also provide (an ugly) way to run a oneoff update rather than whole playbook

--- setup_maw.sh
+++ setup_maw.sh
@@ -4,9 +4,11 @@
 MAW_ENV=$(get_maw_env)
 HOST=$(get_value "Hostname (must exist in inventory file): " 'n')
 FIRST_RUN=$(get_value "First Run - need to prompt for sshd password?  (y/N): " 'n' 'n')
+ONEOFF=$(get_value "Run the one-off playbook?  (y/N): " 'n' 'n')
 VERBOSE=$(get_value "Verbose?  (y/N): " 'n' 'n')
-TAGS=$(get_value "Run specific tag?  (maw_backups | maw_host | maw_services): " 'n' 'n')
-PLAYBOOK_ARGS=("--ask-become-pass")
+TAGS=$(get_value "Run specific tag?  (maw_backups | maw_host | maw_services...): " 'n' 'n')
+ANSIBLE_RUNNER=ansible-playbook
+ANSIBLE_ARGS=("--ask-become-pass")
 
 if [ "${MAW_ENV}" == "prod" ]; then
     INVENTORY="inventories/inventory_prod.yml"
@@ -15,32 +17,42 @@ else
 fi
 
 if [ "${FIRST_RUN}" == 'y' ]; then
-    PLAYBOOK_ARGS+=("--ask-pass")
+    ANSIBLE_ARGS+=("--ask-pass")
 fi
 
 if [ "${VERBOSE}" == 'y' ]; then
-    PLAYBOOK_ARGS+=("-vvv")
+    ANSIBLE_ARGS+=("-vvv")
 fi
 
-if [ "${TAGS}" == 'maw_backups' -o "${TAGS}" == 'maw_host' -o "${TAGS}" == 'maw_services' ]; then
-    PLAYBOOK_ARGS+=("--tags")
-    PLAYBOOK_ARGS+=("${TAGS}")
+if [ "${TAGS}" != 'n' ]; then
+    ANSIBLE_ARGS+=("--tags")
+    ANSIBLE_ARGS+=("${TAGS}")
 fi
 
-PLAYBOOK_ARGS+=("--inventory")
-PLAYBOOK_ARGS+=("${INVENTORY}")
-PLAYBOOK_ARGS+=("--limit")
-PLAYBOOK_ARGS+=("maw_current_prod,${HOST}")
-PLAYBOOK_ARGS+=("--extra-vars")
-PLAYBOOK_ARGS+=("@vars/maw_${MAW_ENV}.yml")
-PLAYBOOK_ARGS+=("playbook_maw.yml")
+ANSIBLE_ARGS+=("--inventory")
+ANSIBLE_ARGS+=("${INVENTORY}")
+ANSIBLE_ARGS+=("--limit")
+ANSIBLE_ARGS+=("maw_current_prod,${HOST}")
+ANSIBLE_ARGS+=("--extra-vars")
+ANSIBLE_ARGS+=("@vars/maw_${MAW_ENV}.yml")
+
+if [ "${ONEOFF}" == 'y' ]; then
+    ANSIBLE_ARGS+=("playbook_maw_oneoff.yml")
+else
+    ANSIBLE_ARGS+=("playbook_maw.yml")
+fi
 
 echo ''
+echo ''
+echo '** this will run the following command **'
+echo "${ANSIBLE_RUNNER} ${ANSIBLE_ARGS[*]}"
+echo ''
+echo ''
 
-DO_RUN=y # $(get_value "Configure -[${MAW_ENV}]- environment for mikeandwan.us on -[${HOST}]-?  (y/N): " 'n' 'n')
+DO_RUN=$(get_value "Configure -[${MAW_ENV}]- environment for mikeandwan.us on -[${HOST}]-?  (y/N): " 'n' 'n')
 
 if [ "${DO_RUN}" != 'y' ]; then
     exit
 fi
 
-ansible-playbook "${PLAYBOOK_ARGS[@]}"
+"${ANSIBLE_RUNNER}" "${ANSIBLE_ARGS[@]}"
